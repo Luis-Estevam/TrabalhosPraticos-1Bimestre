afb625198430e5720a43c5877771860c8f8133a3
Milena <milenaswierk5@gmail.com>
2026-04-10 11:55:53 -0300

    funcoes benchmark

--- src/ProjetoSorts/Benchmark.java
+++ src/ProjetoSorts/Benchmark.java
@@ -1,4 +1,21 @@
 package Trabalhos.src.ProjetoSorts;
 
 public class Benchmark {
+    BubbleSort bubble = new BubbleSort();
+    InsertionSort insertion = new InsertionSort();
+
+    public long MedirBubbleSort (int[] array) {
+        long inicio = System.nanoTime();
+        bubble.bubbleSort(array);
+        long fim = System.nanoTime();
+        return fim - inicio;
+    }
+
+    public long MedirInsertionSort(int[] array) {
+        long inicio = System.nanoTime();
+        insertion.insertionSort(array);
+        long fim = System.nanoTime();
+        return fim - inicio;
+    }
+
 }
